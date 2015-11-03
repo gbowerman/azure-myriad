ccd2d1fbd150be2e70ab66dbbc27a4ee30b141dd
Guy Bowerman <guybo@outlook.com>
2015-11-02 17:48:42 -0800

    Add gethostname to PHP file

--- autoscale/install_lap.sh
+++ autoscale/install_lap.sh
@@ -6,6 +6,7 @@ apt-get -y install apache2 php5
 
 # write some PHP
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
+echo \<\?php gethostname\(\)\; \?\> >> /var/www/html/phpinfo.php
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
 # restart Apache
