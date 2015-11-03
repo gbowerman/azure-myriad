d17be1eec97dfa4a656130bcf4524bbf386c3e95
Guy Bowerman <guybo@outlook.com>
2015-11-02 22:22:39 -0800

    Wait for apt-get to complete

--- autoscale/install_lap.sh
+++ autoscale/install_lap.sh
@@ -1,56 +1,13 @@
 #!/bin/bash
 
-#######################################
-# wait for network to become ready
-#######################################
-ensureAzureNetwork()
-{
-  # ensure the host name is resolvable
-  hostResolveHealthy=1
-  for i in {1..120}; do
-    host $VMNAME
-    if [ $? -eq 0 ]
-    then
-      # hostname has been found continue
-      hostResolveHealthy=0
-      echo "the host name resolves"
-      break
-    fi
-    sleep 1
-  done
-  if [ $hostResolveHealthy -ne 0 ]
-  then
-    echo "host name does not resolve, aborting install"
-    exit 1
-  fi
-
-  # ensure the network works
-  networkHealthy=1
-  for i in {1..12}; do
-    wget -O/dev/null http://bing.com
-    if [ $? -eq 0 ]
-    then
-      # hostname has been found continue
-      networkHealthy=0
-      echo "the network is healthy"
-      break
-    fi
-    sleep 10
-  done
-  if [ $networkHealthy -ne 0 ]
-  then
-    echo "the network is not healthy, aborting install"
-    ifconfig
-    ip a
-    exit 2
-  fi
-}
-ensureAzureNetwork
+while [ ps aux | grep -v grep | grep "apt-get" > /dev/null ]; do
+    sleep 2
+done
 
 apt-get -y update
 
 # install Apache and PHP
-apt-get -y install apache2 php5  
+apt-get -y install apache2 php5
 
 # write some PHP
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
@@ -59,3 +16,4 @@ echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
 # restart Apache
 apachectl restart
+
