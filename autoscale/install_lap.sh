5a7353b4b1c98734db4821ed11a175f5df450b25
Guy Bowerman <guybo@outlook.com>
2015-11-03 15:46:37 -0800

    Fix php script

--- autoscale/install_lap.sh
+++ autoscale/install_lap.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
+# wait for Linux Diagnostic Extension to complete
 while ( ! grep "Start mdsd" /var/log/azure/Microsoft.OSTCExtensions.LinuxDiagnostic/2.1.5/extension.log); do
     sleep 5
 done
 
-apt-get -y update
-
 # install Apache and PHP
-
-while ps aux | grep -v grep | grep "apt-get" > /dev/null; do
-    sleep 2
-done
+apt-get -y update
 apt-get -y install apache2 php5
 
 # write some PHP
-\<\?php \$hostname = gethostname\(\)\; \?\> > /var/www/html/index.php
-\<center\>\<h1\>Scale Set App - \<\?php echo \"\$hostname\"\;\?\>\</h1\>\</center\> >> /var/www/html/index.php
-\<br/\>\<br/\>\<br/\> >> /var/www/html/index.php
-\<\?php phpinfo\(\)\; \?\> >> /var/www/html/index.php
-
+echo \<\?php \$hostname = gethostname\(\)\; \?\> > /var/www/html/index.php
+echo \<center\>\<h1\>Scale Set App - \<\?php echo \"\$hostname\"\;\?\>\</h1\>\</center\> >> /var/www/html/index.php
+echo \<br/\>\<br/\>\<br/\> >> /var/www/html/index.php
+echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/index.php
+rm /var/www/html/index.html
 # restart Apache
 apachectl restart
 
