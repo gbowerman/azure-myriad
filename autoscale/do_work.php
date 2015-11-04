239f99b788da523e2e7c9907836c1e4387508b86
Guy Bowerman <guybo@outlook.com>
2015-11-04 14:11:02 -0800

    Clean-up do_work

--- autoscale/do_work.php
+++ autoscale/do_work.php
@@ -1,19 +1,21 @@
 <html>
-<head><title>CPU load example</title></head>
-<body>
-<br/><br/>
-<?php
-if ($_SERVER['REQUEST_METHOD'] == "POST")
-{
-    $num = $_POST["num"];
-	for ($x = 0; $x <= $num * 5000000; $x++) 
-	{
-      $var = $num+1;
-	}
-} 
-?>
-<center>
-<h2>Processing complete</h2>
-</center>
-</body>
+  <head><title>CPU load example</title></head>
+  <body>
+    <br/><br/>
+    <center>
+    <h2>Doing work..</h2>
+    <br/><br/>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        $num = $_POST["num"];
+	    for ($x = 0; $x <= $num * 5000000; $x++) 
+	    {
+          $var = $num+1;
+	    }
+    } 
+    ?>
+    <h2>Processing complete</h2>
+    </center>
+  </body>
 </html>
