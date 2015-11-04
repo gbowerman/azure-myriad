f850641529e012f79e8ecd2150ae7cbd9d6d7c64
Guy Bowerman <guybo@outlook.com>
2015-11-04 10:42:44 -0800

    Add sample PHP file
    1st step in moving from embedded to downloaded PHP app.

---
+++ autoscale/index.php
@@ -0,0 +1,11 @@
+<html>
+  <head><title>Scale Set Autoscale demo app</title></head>
+  <body>
+    <?php $hostname = gethostname(); ?>
+    <center>
+	  <h1>Scale Set App - <?php echo "$hostname";?></h1>
+	</center>
+    <br/><br/><br/>
+    <?php phpinfo(); ?>
+  </body>
+</html>
