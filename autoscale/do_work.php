b65ac2e38aeeebdaeb24b16162d88f64fab71c47
Guy Bowerman <guybo@outlook.com>
2015-11-05 12:06:42 -0800

    Formatting

--- autoscale/do_work.php
+++ autoscale/do_work.php
@@ -3,19 +3,19 @@
   <body>
     <br/><br/>
     <center>
-	<h2><?php $hostname = gethostname(); ?> &nbsp;Doing work..</h2>
+    <h2><?php $hostname = gethostname(); ?> &nbsp;Doing work..</h2>
     <br/><br/>
     <?php
-	set_time_limit(0);
+    set_time_limit(0);
 
     if ($_SERVER['REQUEST_METHOD'] == "POST")
     {
-        $num = $_POST["num"];
-	    for ($x = 0; $x <= $num * 5000000; $x++) 
+      $num = $_POST["num"];
+	    for ($x = 0; $x <= $num * 5000000; $x++)
 	    {
-          $var = $num+1;
+        $var = $num+1;
 	    }
-    } 
+    }
     ?>
     <h2>Processing complete</h2>
     </center>
