dccc9db54fe7bcf7dbdf2b2e4782122dff3b8bd0
Guy Bowerman <guybo@outlook.com>
2015-11-05 15:36:05 -0800

    Tune iteration time

--- autoscale/do_work.php
+++ autoscale/do_work.php
@@ -17,7 +17,7 @@
     if ($_SERVER['REQUEST_METHOD'] == "GET")
     {
       $num = $_GET["num"];
-      for ($x = 0; $x <= $num * 5000000; $x++)
+      for ($x = 0; $x <= $num * 3900000; $x++)
       {
         $var = $num+1;
       }
