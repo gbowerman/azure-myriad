b65f4d5489b32f97388199d8617d8b4ad7c05200
Guy Bowerman <guybo@outlook.com>
2017-06-14 15:16:14 -0700

    Add parameters for Azure files share

--- azure-files/mountazurefiles.sh
+++ azure-files/mountazurefiles.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-workserver_path=/srv/workserver
-mkdir $workserver_path
-cp workserver.py $workserver_path
+# $1 = Azure storage account name
+# $2 = Azure storage account key
+# $3 = Azure file name
 
-# install python3-bottle 
-apt-get -y update
-apt-get -y install python3-bottle
-
-# create a service
-touch /etc/systemd/system/workserver.service
-printf '[Unit]\nDescription=workServer Service\nAfter=rc-local.service\n' >> /etc/systemd/system/workserver.service
-printf '[Service]\nWorkingDirectory=%s\n' $workserver_path >> /etc/systemd/system/workserver.service
-printf 'ExecStart=/usr/bin/python3 %s/workserver.py\n' $workserver_path >> /etc/systemd/system/workserver.service
-printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/workserver.service
-printf '[Install]\nWantedBy=multi-user.target\nAlias=workserver.service' >> /etc/systemd/system/workserver.service
-chmod +x /etc/systemd/system/workserver.service
-
-systemctl start workserver
+# put your bash script here
 
 
