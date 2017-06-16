28846771e39a027f411e9199b3ef17f164f31524
Guy Bowerman <guybo@outlook.com>
2017-06-16 08:13:24 -0700

    Remove duplicate #!/bin/bash

--- azure-files/mountazurefiles.sh
+++ azure-files/mountazurefiles.sh
@@ -4,22 +4,20 @@
 # $3 = Azure file share name
 # $4 = mountpoint path
 
-
-#!/bin/bash   
-
-# See for details https://azure.microsoft.com/en-us/documentation/articles/storage-how-to-use-files-linux/
+# For more details refer to https://azure.microsoft.com/en-us/documentation/articles/storage-how-to-use-files-linux/
 
 storageaccountname=$1
 storageaccountkey=$2
 filesharename=$3
 mountpointpath=$4
 
+apt-get -y update
+
 # Check for distro use proper command to unstall CIFS Utils
 # Check if user is on-cloud or on-prem - Check for SMB 3.0 or 2.1
 # Check if storage account and VM are in same region
 # Mapping for distro to SMB support mapping
 # Check if user is on-prem - Check for encryption support (check for kernel versin > 4.11 or greater Ubintu, SUSE)
-
 
 sudo apt-get install cifs-utils
 mkdir "$mountpointpath"
