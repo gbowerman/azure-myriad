2444d630eeddc42452cd0f9e69a185ddb5ce7297
Guy Bowerman <guybo@outlook.com>
2017-06-27 11:27:44 -0700

    Adding support for symlink

--- azure-files/mountazurefiles.sh
+++ azure-files/mountazurefiles.sh
@@ -5,6 +5,7 @@
 # $2 = Azure storage account key
 # $3 = Azure file share name
 # $4 = mountpoint path
+# $5 - username
 
 # For more details refer to https://azure.microsoft.com/en-us/documentation/articles/storage-how-to-use-files-linux/
 
@@ -19,6 +20,10 @@ apt-get -y update
 apt-get install cifs-utils
 mkdir $4
 mount -t cifs //$1.file.core.windows.net/$3 $4 -o vers=3.0,username=$1,password=$2,dir_mode=0755,file_mode=0664
+
+# create a symlink from /mountpath/xxx to /u/username/xxx
+linkpoint=`echo $4 | sed 's/.*\///'``
+ln -s $4 /u/$5/$linkpoint
 
 #Creating a dummy marker files for testing
 echo "hello from $HOSTNAME" > $4/$HOSTNAME.txt                                                                         
