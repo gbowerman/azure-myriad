62c93fed2b3e360b81cd21773cb82858d73103d5
Guy Bowerman <guybo@outlook.com>
2017-06-27 14:24:05 -0700

    Use eval to identify user's homedir

--- azure-files/mountazurefiles.sh
+++ azure-files/mountazurefiles.sh
@@ -21,9 +21,9 @@ apt-get install cifs-utils
 mkdir $4
 mount -t cifs //$1.file.core.windows.net/$3 $4 -o vers=3.0,username=$1,password=$2,dir_mode=0755,file_mode=0664
 
-# create a symlink from /mountpath/xxx to /u/username/xxx
+# create a symlink from /mountpath/xxx to ~username/xxx
 linkpoint=`echo $4 | sed 's/.*\///'`
-ln -s $4 /u/$5/$linkpoint
+eval ln -s $4 ~$5/$linkpoint
 
 #Creating a dummy marker files for testing
 echo "hello from $HOSTNAME" > $4/$HOSTNAME.txt                                                                         
